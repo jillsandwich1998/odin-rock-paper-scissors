30649ce8f3d3728b319757b930a885f4b4852e8e
jillsandwich1998 <davidias1000@gmail.com>
2023-08-21 11:05:47 -0300

    Separa o código em funções individuais

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,17 @@
+//Variáveis Globais
+let paragraphFragment = document.createDocumentFragment();
+let roundNumber = 0; //Contador para as partidas;
+let playerWins = 0; // Variável para contagem de vitórias do jogador
+let computerWins = 0; // Variável para contagem de vitórias do computador
+let restartButton; //Botão de reset do jogo
+let playerText;
+let computerText;
+let winText;
+let resultText;
+const divPrincipal = document.getElementsByClassName("main-content");
+let divBotoes = document.createElement("div");
+let buttons = document.getElementsByClassName("btn");
+
 function getComputerChoice() {
   let computerSelection = Math.round(Math.random() * 1) + 1;
   console.log(
@@ -6,30 +20,42 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-function game() {
-  let roundNumber = 1; //Contador para as partidas;
-  //Getting Buttons
-  let buttons = document.getElementsByClassName("btn");
+function playerSelectionChooser(event) {
+  const clickedButton = event.target;
+  const computerSelection = getComputerChoice();
+  const buttonClass = clickedButton.classList;
 
+  let playerSelection;
+  if (buttonClass.contains("rock")) {
+    playerSelection = 0;
+  } else if (buttonClass.contains("paper")) {
+    playerSelection = 1;
+  } else if (buttonClass.contains("scissors")) {
+    playerSelection = 2;
+  }
+
+  playRound(playerSelection, computerSelection);
+}
+
+function addPlayEvents() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", playerSelectionChooser);
+  }
+}
+
+function createGameElements() {
   //Criando os parágrafos que irão exibir as informações sobre o jogo
-  let playerText = document.createElement("p");
-  let computerText = document.createElement("p");
-  let resultText = document.createElement("p");
-  let winText = document.createElement("p");
+  playerText = document.createElement("p");
+  computerText = document.createElement("p");
+  resultText = document.createElement("p");
+  winText = document.createElement("p");
 
-  let restartButton = document.createElement("button");
+  restartButton = document.createElement("button");
   restartButton.textContent = "RESTART GAME";
   restartButton.className = "restartButton";
   restartButton.style.padding = "40px";
 
   winText.classList.add("win-text");
-
-  // Adicione uma classe aos parágrafos criados
-  let playerWins = 0; // Variável para contagem de vitórias do jogador
-  let computerWins = 0; // Variável para contagem de vitórias do computador
-
-  // Criando um fragmento de documento para os parágrafos
-  let paragraphFragment = document.createDocumentFragment();
 
   // Adicionando os elementos de texto ao fragmento
   paragraphFragment.appendChild(playerText);
@@ -38,103 +64,123 @@ function game() {
   paragraphFragment.appendChild(winText);
 
   // Criando div que armazenrá os textos
-  let divBotoes = document.createElement("div");
+
   divBotoes.classList.add("game-info");
 
   //Adiciona toda a info de texto à div criada de uma vez só
   divBotoes.appendChild(paragraphFragment);
 
   // Adicionando o fragmento com os parágrafos ao body da página
-  let divPrincipal = document.getElementsByClassName("main-content");
   divPrincipal[0].appendChild(divBotoes);
+}
 
-  function playRound(playerSelection, computerSelection) {
-    //ROCK == 0
-    //PAPER == 1
-    //SCISSORS == 2
+// MOSTRA OS RESULTADOS NA TELA
+function displayRoundResults(playerSelection, computerSelection) {
+  //ROCK == 0
+  //PAPER == 1
+  //SCISSORS == 2
 
-    if (playerSelection === 0 && computerSelection === 1) {
-      resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE PAPER AND YOU CHOSE ROCK`;
-      computerWins++;
-    } else if (playerSelection === 0 && computerSelection === 2) {
-      resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE ROCK AND COMPUTER CHOSE SCISSORS`;
-      playerWins++;
-    } else if (playerSelection === 1 && computerSelection === 0) {
-      resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE PAPER AND COMPUTER CHOSE ROCK`;
-      playerWins++;
-    } else if (playerSelection === 1 && computerSelection === 2) {
-      resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE SCISSORS AND YOU CHOSE PAPER`;
-      computerWins++;
-    } else if (playerSelection === 2 && computerSelection === 0) {
-      resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE ROCK AND YOU CHOSE SCISSORS`;
-      computerWins++;
-    } else if (playerSelection === 2 && computerSelection === 1) {
-      resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE SCISSORS AND COMPUTER CHOSE PAPER`;
-      playerWins++;
-    } else {
-      resultText.textContent = `Round ${roundNumber}: IT'S A TIE! - BOTH CHOSE THE SAME`;
-    }
-
-    // Atualize o texto das vitórias do jogador e do computador
-    playerText.textContent = `Player Score: ${playerWins}`;
-    computerText.textContent = `Computer Score: ${computerWins}`;
-
-    //Checa para ver se alguém já ganhou o jogo
-    if (computerWins === 5 || playerWins === 5) {
-      if (computerWins === 5) {
-        winText.textContent = "COMPUTER WON THE GAME! - THANKS FOR PLAYING!";
-      } else if (playerWins === 5) {
-        winText.textContent = "PLAYER WON THE GAME! - THANKS FOR PLAYING!";
-      }
-
-      divPrincipal[0].appendChild(restartButton);
-      //Evento que acontecerá quando o botão "Restart Game" for clicado
-      restartButton.addEventListener("click", function (event) {
-        let textos = document.querySelectorAll(".main-content p");
-        textos.forEach((texto) => {
-          divBotoes.removeChild(texto);
-        });
-        //Zera as variáveis que armazenam os resultados do jogo
-        playerWins = 0;
-        computerWins = 0;
-        roundNumber=1;
-        resultText.textContent = "";
-        winText.textContent = "";
-        restartButton.remove();
-      });
-    }
-  }
-
-  //START ROUND
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function (event) {
-      //AÇÃO EXECUTADA QUANDO O BOTÃO É CLICADO
-      let clickedButton = event.target;
-      let computerSelection = getComputerChoice();
-
-      //Obtendo as classes do botão que foi clicado pelo usuário
-      let buttonClass = clickedButton.classList;
-
-      if (buttonClass.contains("rock")) {
-        playerSelection = 0;
-      } else if (buttonClass.contains("paper")) {
-        playerSelection = 1;
-      } else if (buttonClass.contains("scissors")) {
-        playerSelection = 2;
-      }
-
-      if (roundNumber === 1) {
-        paragraphFragment.appendChild(playerText);
-        paragraphFragment.appendChild(computerText);
-        paragraphFragment.appendChild(resultText);
-        paragraphFragment.appendChild(winText);
-        divBotoes.appendChild(paragraphFragment);
-      }
-      console.log(`Player selected: ${playerSelection}`);
-      playRound(playerSelection, computerSelection, roundNumber);
-      roundNumber++;
-    });
+  if (playerSelection === 0 && computerSelection === 1) {
+    resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE PAPER AND YOU CHOSE ROCK`;
+    computerWins++;
+  } else if (playerSelection === 0 && computerSelection === 2) {
+    resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE ROCK AND COMPUTER CHOSE SCISSORS`;
+    playerWins++;
+  } else if (playerSelection === 1 && computerSelection === 0) {
+    resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE PAPER AND COMPUTER CHOSE ROCK`;
+    playerWins++;
+  } else if (playerSelection === 1 && computerSelection === 2) {
+    resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE SCISSORS AND YOU CHOSE PAPER`;
+    computerWins++;
+  } else if (playerSelection === 2 && computerSelection === 0) {
+    resultText.textContent = `Round ${roundNumber}: COMPUTER WINS! - HE CHOSE ROCK AND YOU CHOSE SCISSORS`;
+    computerWins++;
+  } else if (playerSelection === 2 && computerSelection === 1) {
+    resultText.textContent = `Round ${roundNumber}: YOU WIN! - YOU CHOSE SCISSORS AND COMPUTER CHOSE PAPER`;
+    playerWins++;
+  } else {
+    resultText.textContent = `Round ${roundNumber}: IT'S A TIE! - BOTH CHOSE THE SAME`;
   }
 }
 
-game();
+function displayNewScore(playerWins, computerWins) {
+  // Atualize o texto das vitórias do jogador e do computador
+  playerText.textContent = `Player Score: ${playerWins}`;
+  computerText.textContent = `Computer Score: ${computerWins}`;
+}
+
+function initializeGame() {
+  createGameElements();
+
+  let computerSelection;
+  let playerSelection;
+  let buttonClass;
+  let clickedButton;
+
+  //Adiciona eventos aos botões de escolhas
+  addPlayEvents();
+}
+
+function playRound(playerSelection, computerSelection) {
+  roundNumber++;
+  console.log("Round Number = " + roundNumber);
+  //Irá exibir os resultados do round atual na tela e incrementará o número de vitórias do jogador
+  displayRoundResults(playerSelection, computerSelection);
+
+  //Atualiza as informações da tela sobre o score de cada um dos jogadores
+  displayNewScore(playerWins, computerWins);
+
+  //Checa se um dos jogadores já atingiu 5 vitórias, se sim, finaliza o game, se não o jogo continua.
+  if (computerWins === 5 || playerWins === 5) {
+    endGame(playerWins, computerWins);
+  }
+}
+
+function restartGame() {
+  // Remove o botão de reinício, se existir
+  if (restartButton) {
+    restartButton.remove();
+  }
+
+  // Remove os elementos de texto do jogo
+  let textos = divBotoes.querySelectorAll("p");
+  textos.forEach((texto) => {
+    divBotoes.removeChild(texto);
+  });
+
+  // Zera as variáveis do jogo
+  playerWins = 0;
+  computerWins = 0;
+  roundNumber = 0;
+
+  //Adiciona os eventos ao jogo novamente
+  addPlayEvents();
+
+  // Recria os elementos de texto e adiciona ao fragmento
+  createGameElements();
+}
+
+//FUNÇÃO FINALIZA O JOGO
+function endGame(playerWins, computerWins) {
+  // Checa para ver se alguém já ganhou o jogo
+  if (computerWins === 5 || playerWins === 5) {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].removeEventListener("click", playerSelectionChooser);
+    }
+
+    if (computerWins === 5) {
+      winText.textContent = "COMPUTER WON THE GAME! - THANKS FOR PLAYING!";
+    } else if (playerWins === 5) {
+      winText.textContent = "PLAYER WON THE GAME! - THANKS FOR PLAYING!";
+    }
+
+    // Adiciona o botão de reinício
+    divPrincipal[0].appendChild(restartButton);
+
+    // Define o evento de clique no botão de reinício
+    restartButton.addEventListener("click", restartGame);
+  }
+}
+
+//INICIALIZAÇÃO DO GAME
+initializeGame();
